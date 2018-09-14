e5e92c67dc8692e41e73210fb55a2330094e8f52
Ivan Kuzmin <kuzmin6969@gmail.com>
2018-09-14 14:54:21 +0300

    Install Vuetify

---
+++ src/views/DetailView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card-image">
+    Detail
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="list">
+    Home
+  </ul>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+  .list {
+    width: 100%;
+    padding: 0;
+  }
+</style>
